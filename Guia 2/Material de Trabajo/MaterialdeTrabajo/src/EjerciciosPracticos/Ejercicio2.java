264a492b7592a929ec957c278836bbdb2decc581
“Mariela <melodyesteche@gmail.com>
2023-03-22 17:21:43 -0300

    Guia 2 Java

---
+++ Guia 2/Material de Trabajo/MaterialdeTrabajo/src/EjerciciosPracticos/Ejercicio2.java
@@ -0,0 +1,30 @@
+/*
+ *Ejercicio 2
+ *¿Recuerdas las variables que creaste en el ejercicio anterior?
+ *Ahora deberás asignarles un valor.
+
+ */
+package EjerciciosPracticos;
+
+/**
+ *
+ * @author TKBio
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte entConSigno8 = 127;
+        short entConSigno16 = 32767;
+        int entConSigno32 = 214748664;
+        long entConSigno64 = 9223372036854775807L;
+        float decimales32 = 754;
+        double decimales64 = 754;
+        boolean VoF = true;
+        char caracterUnicode = '\u00E1';// esto es 'a'
+        String nombre = "Mel";
+    }
+}
---
+++ Guia 2/Material de Trabajo/MaterialdeTrabajo/src/EjerciciosPracticos/Ejercicio4.java
@@ -0,0 +1,25 @@
+/*
+Ejercicio 4
+Define una variable que aloje tu nombre y otra que guarde tu edad. 
+Imprime ambas variables por pantalla.
+Recomendamos que hagan el siguiente experimento: 
+tipear en minúsculas la palabra sout + tab
+Esto nos va a generar un System.out.println()
+ */
+package EjerciciosPracticos;
+
+/**
+ *
+ * @author TKBio
+ */
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        //Defino la variable nombre y edad
+        String nombre = "Mariela Esteche";
+        int anio = 40;
+        //imprimo el resultado
+        System.out.println("Hola! Mi nombre es " + nombre);
+        System.out.println("Tengo " + anio + " años! ");
+    }
+}
